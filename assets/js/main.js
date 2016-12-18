1d45790b73203c42721e588e11de3a82f8afe0a5
Muhammed Zaim <info@muhammedzaim.com>
2016-12-18 15:14:17 +0200

    Update main.js

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+<script type='text/javascript'>
+    function encode() {
+        var filesSelected = document.getElementById("image").files;
+        if (filesSelected.length > 0) {
+            var fileToLoad = filesSelected[0];
+            var fileReader = new FileReader();
+            fileReader.onload = function(fileli) {
+                var data = fileli.target.result;
+                var Image2 = document.createElement('img');
+                Image2.src = data;
+                document.getElementById("test").innerHTML = Image2.src;
+                console.log("200 OK");
+            }
+            fileReader.readAsDataURL(fileToLoad);
+        }
+    }
+</script>
